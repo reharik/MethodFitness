e70aec08bea7a7d66aeacd3e74cf6e030cafdafd
Raif Harik <reharik@gmail.com>
2017-05-27 11:06:28 -0500

    trying to get workflow tests working again, no luck

--- workflows/app/registry_test.js
+++ workflows/app/registry_test.js
@@ -1,28 +1,32 @@
-/**
- * Created by parallels on 9/3/15.
- */
+
 let dagon = require('dagon');
 let path = require('path');
+
 module.exports = function(_options) {
   let options = _options || {};
   let container = dagon(options.dagon).container;
   let result;
   try {
-    result = new container(x=> x.pathToRoot(path.join(__dirname, '..'))
-            .requireDirectoryRecursively('./app/src')
-            .requireDirectoryRecursively('./app/tests/unitTests/mocks')
-                .for('eventstore').require('./app/tests/unitTests/mocks/eventStoreMock')
-                .for('corelogger').renameTo('logger')
-                .for('ramda').renameTo('R')
-                .for('ramdafantasy').renameTo('_fantasy')
-                .for('bluebird').renameTo('Promise')
-                .for('eventstore').replaceWith('eventStoreMock')
-                .for('appfuncs').replaceWith('applicationFunctionsPlugin')
-                .complete(),
-                x=>x.instantiate('eventstore').asFunc().withParameters(options.children || {})
-                .instantiate('eventhandlerbase').asFunc().withParameters(options.children || {})
-                .complete());
-
+    result = container(
+      x=> x.pathToRoot(path.join(__dirname, '/../'))
+        .requireDirectoryRecursively('./app/src')
+        .requireDirectoryRecursively('./app/tests/unitTests/mocks')
+        .groupAllInDirectory('./app/src/CommandHandlers', 'CommandHandlers_array')
+        .requiredModuleRegistires(['ges-eventsourcing'])
+        .for('eventstore').require('./app/tests/unitTests/mocks/eventStoreMock')
+        .for('corelogger').renameTo('logger')
+        .for('ramda').renameTo('R')
+        .for('ramdafantasy').renameTo('_fantasy')
+        .for('bluebird').renameTo('Promise')
+        .for('applicationFunctions').renameTo('appfuncs')
+        .for('eventstore').replaceWith('eventStoreMock')
+        .complete(),
+      x=>x.instantiate('eventstore').asFunc().withParameters(options.children || {})
+        .instantiate('pgFuture').asFunc().withParameters(options.children || {})
+        .instantiate('eventRepository').asFunc().withParameters(options.children || {})
+        .instantiate('gesConnection').asFunc().withParameters(options.children || {})
+        .instantiate('logger').asFunc().withParameters(options.logger || {})
+        .complete());
   } catch (ex) {
     console.log(ex);
     console.log(ex.stack);
