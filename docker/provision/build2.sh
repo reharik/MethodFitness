f5e0940dd1a2a1992d1b48d177248c93e18db1cd
Raif Harik <reharik@gmail.com>
2017-06-28 17:42:27 -0500

    working on deployment

--- docker/provision/build2.sh
+++ docker/provision/build2.sh
@@ -11,7 +11,7 @@
 set -e
 
  echo "Logging into the ECR"
- $(aws ecr get-login --region us-east-2)
+ $(sudo aws ecr get-login --region us-east-2)
 
 echo "Creating the Build artifacts directory"
 rm -f docker/.envrc
