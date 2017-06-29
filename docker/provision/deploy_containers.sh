cbe1b7ca80cc95c964372273c665ba1b9c0d9c6b
Raif Harik <reharik@gmail.com>
2017-06-29 15:14:45 -0500

    more work on deploy

--- docker/provision/deploy_containers.sh
+++ docker/provision/deploy_containers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#$(aws ecr get-login --region us-east-2)
 
 cat ./.envrc
+echo cat ./.envrc
 docker run hello-world
 docker-compose -p methodfitness down --rmi local
 docker-compose -p methodfitness up -d
