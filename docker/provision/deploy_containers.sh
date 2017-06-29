4438cb894924b9322cf04859eaf8828b1e193906
Raif Harik <reharik@gmail.com>
2017-06-29 14:13:58 -0500

    more work on deploy

--- docker/provision/deploy_containers.sh
+++ docker/provision/deploy_containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 $(aws ecr get-login --region us-east-2)
-docker-compose -p methodfitness down --rmi local
-docker-compose -p methodfitness up -d
+sudo docker-compose -p methodfitness down --rmi local
+sudo docker-compose -p methodfitness up -d
