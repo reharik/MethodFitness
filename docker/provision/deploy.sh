e3f64fe66b7bbd8da458ac610de2dc8fe0b63e9e
Raif Harik <reharik@gmail.com>
2017-06-29 13:43:01 -0500

    more work on deploy

--- docker/provision/deploy.sh
+++ docker/provision/deploy.sh
@@ -4,9 +4,9 @@
 $(aws ecr get-login --region us-east-2)
 
 echo "Uploading artifacts"
-scp docker-compose-deploy.yml ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/docker-compose.yml
-scp .env ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/.env
-scp deploy.sh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/deploy_containers.sh
+scp docker/docker-compose-deploy.yml ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/docker-compose.yml
+scp docker/.env ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/.env
+scp docker/provision/deploy.sh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/deploy_containers.sh
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x docker-compose.yml
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x deploy_containers.sh
 
