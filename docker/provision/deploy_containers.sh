b25736c639a1543ab3e809f57660df947c93945d
Raif Harik <reharik@gmail.com>
2017-07-01 16:04:00 -0500

    blasting all images now

--- docker/provision/deploy_containers.sh
+++ docker/provision/deploy_containers.sh
@@ -4,7 +4,7 @@ $(aws ecr get-login --region us-east-2)
 
 cat ./.env
 
-- sudo docker rm -f $$(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
-- sudo docker rmi $$(docker images -a) 2>/dev/null || echo "No more containers to remove."
+ sudo docker rm -f $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
+ sudo docker rmi $(docker images -a) 2>/dev/null || echo "No more containers to remove."
 
 sudo docker-compose -p methodfitness up -d
