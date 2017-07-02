1591cad9015167599e859d66e5d0215579ec1903
Raif Harik <reharik@gmail.com>
2017-07-02 16:04:57 -0500

    removing volume

--- docker/provision/deploy_containers.sh
+++ docker/provision/deploy_containers.sh
@@ -4,7 +4,7 @@ $(aws ecr get-login --region us-east-2)
 
 cat ./.env
 
- sudo docker rm -f $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
+ sudo docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
  sudo docker images | grep "/methodfitness" | awk '{print $1 ":" $2}' | xargs docker rmi
 
 sudo docker-compose -p methodfitness up -d
