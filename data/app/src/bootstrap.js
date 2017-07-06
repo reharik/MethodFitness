2d283cd6af3ae07c3de3b4fc03326ac645ee32c5
Raif Harik <reharik@gmail.com>
2017-07-06 11:10:56 -0500

    some changes

--- data/app/src/bootstrap.js
+++ data/app/src/bootstrap.js
@@ -1,6 +1,8 @@
 module.exports = function(migration,seedES) {
   return async function () {
+    let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
     await migration(true);
+    await wait(1000)
     await seedES();
   };
 };
