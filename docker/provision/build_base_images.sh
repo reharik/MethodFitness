2506bea1d46d877c40afd72392fdd694b9b3b1c9
Raif Harik <reharik@gmail.com>
2018-01-07 17:42:17 -0600

    getting better

--- docker/provision/build_base_images.sh
+++ docker/provision/build_base_images.sh
@@ -18,7 +18,9 @@ echo "Removing old images"
 echo "--------------------------------------"
 
      docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
-     docker images | grep "/base_mf" | awk '{print $3}' | xargs -r docker rmi -f
+     docker rmi $(docker images -f "label=cleanMe" -q)
+
+#     docker images | grep "/base_mf" | awk '{print $3}' | xargs -r docker rmi -f
 
 echo "--------------------------------------"
 echo "building and pushing the base images"
