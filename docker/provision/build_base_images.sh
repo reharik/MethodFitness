34d141bcbfa2f5a1155e88146d1c036aa2e5d70e
Raif Harik <reharik@gmail.com>
2018-01-07 15:50:00 -0600

    trying to get bash syntax right

--- docker/provision/build_base_images.sh
+++ docker/provision/build_base_images.sh
@@ -18,7 +18,7 @@ echo "Removing old images"
 echo "--------------------------------------"
 
      docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
-     docker images | grep "/base_mf" | awk '{print $1 ":" $2}' | xargs docker rmi || ''
+     docker images | grep "/base_mf" | awk '{print $1 ":" $2}' | xargs -r docker rmi
 
 echo "--------------------------------------"
 echo "building and pushing the base images"
