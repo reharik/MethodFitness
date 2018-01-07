30c8ccc530e30c2510c6932e36796a072d0103f3
Raif Harik <reharik@gmail.com>
2018-01-07 16:04:32 -0600

    this time I think I got it

--- docker/provision/build_base_images.sh
+++ docker/provision/build_base_images.sh
@@ -18,7 +18,7 @@ echo "Removing old images"
 echo "--------------------------------------"
 
      docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
-     docker images | grep "/base_mf" | awk '{print $1 ":" $2}' | xargs -r docker rmi
+     docker images | grep "/base_mf" | awk '{print $3}' | xargs -r docker rmi
 
 echo "--------------------------------------"
 echo "building and pushing the base images"
@@ -27,14 +27,14 @@ echo "--------------------------------------"
 #         docker rmi $(docker images -q --filter=reference=709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend)
 #         docker rmi $(docker images -q --filter=reference=709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty)
 #         docker rmi $(docker images -q --filter=reference=709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty)
-#         cd docker/node_base && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node:latest
-#         -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node:$$(git show -s --format=%h) .
-#         docker push 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node
-         cd docker/base_mf_firstparty && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty:$$(git show -s --format=%h) .
+         cd docker/node_base && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node:latest
+         -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node:$(git show -s --format=%h) .
+         docker push 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_node
+         cd ../docker/base_mf_firstparty && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty:$(git show -s --format=%h) .
          docker push 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_firstparty
-         cd ../docker/base_mf_thirdparty && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty:$$(git show -s --format=%h) .
+         cd ../docker/base_mf_thirdparty && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty:$(git show -s --format=%h) .
          docker push 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_thirdparty
-         cd ../docker/base_mf_frontend && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend:$$(git show -s --format=%h) .
+         cd ../docker/base_mf_frontend && docker build --no-cache -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend:latest -t 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend:$(git show -s --format=%h) .
          docker push 709865789463.dkr.ecr.us-east-2.amazonaws.com/base_mf_frontend
 
 echo "--------------------------------------"
