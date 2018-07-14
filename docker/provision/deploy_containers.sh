9717b01ee8bd9243ff72005d52eacf914dbdc40c
Raif Harik <reharik@gmail.com>
2018-07-14 12:50:40 -0500

    stupd config code is hardcoded to local

--- docker/provision/deploy_containers.sh
+++ docker/provision/deploy_containers.sh
@@ -6,8 +6,11 @@ cat ./.env
 echo "--------------------------------------"
 echo "Cleaning images on server"
 echo "--------------------------------------"
- sudo docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
- sudo docker images | grep "/methodfitness" | awk '{print $1 ":" $2}' | xargs docker rmi
+docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
+     docker images -q -f "label=methodfitness=child" | while read -r image; do docker rmi -f $image; done;
+     docker images -q -f "label=methodfitness=base3" | while read -r image; do docker rmi -f $image; done;
+     docker images -q -f "label=methodfitness=base2" | while read -r image; do docker rmi -f $image; done;
+     docker images -q -f "label=methodfitness=base1" | while read -r image; do docker rmi -f $image; done;
 echo "--------------------------------------"
 echo "Building new containers"
 echo "--------------------------------------"
