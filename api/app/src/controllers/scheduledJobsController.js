0777c540b30d1e952e544b4d80dae58013c1a03a
Raif Harik <reharik@gmail.com>
2018-07-25 14:39:56 -0500

    fixed the problem with clientAttendsAppointment, need cypress test on this functionality

--- api/app/src/controllers/scheduledJobsController.js
+++ api/app/src/controllers/scheduledJobsController.js
@@ -1,31 +1,43 @@
-module.exports = function(rsRepository,
-                          eventstore,
-                          moment,
-                          uuid,
-                          commands,
-                          logger) {
+module.exports = function(
+  rsRepository,
+  eventstore,
+  moment,
+  uuid,
+  commands,
+  logger,
+) {
   let appointmentStatusUpdate = async function(ctx) {
     logger.debug('arrived at scheduledJobs.appointmentStatusUpdate');
-    let date = moment().weekday(0).format('YYYY-MM-DD');
+    let date = moment()
+      .weekday(0)
+      .format('YYYY-MM-DD');
     let sql = `select * from appointment 
-    where date<='${date}' and date>'${moment().subtract(1, 'month').format('YYYY-MM-DD')}';`;
+    where date<='${date}' and date>'${moment()
+      .subtract(1, 'month')
+      .format('YYYY-MM-DD')}';`;
     const appointments = await rsRepository.query(sql);
 
     logger.info(`appoinments: ${JSON.stringify(appointments)}`);
     let _commands = [];
 
-    appointments.filter(x => {
-      const before = moment(x.endTime).isBefore(moment(), 'minute');
-      const notCompleted = !x.completed;
-      return before && notCompleted;
-    }).forEach(x =>
-      x.clients.forEach(y =>
-        _commands.push(commands.clientAttendsAppointmentCommand({
-          clientId: y,
-          appointmentId: x.appointmentId,
-          appointmentType: x.appointmentType
-        })))
-    );
+    appointments
+      .filter(x => {
+        const before = moment(x.endTime).isBefore(moment(), 'minute');
+        const notCompleted = !x.completed;
+        return before && notCompleted;
+      })
+      .forEach(x =>
+        x.clients.forEach(y =>
+          _commands.push(
+            commands.clientAttendsAppointmentCommand({
+              clientId: y,
+              trainerId: x.trainerId,
+              appointmentId: x.appointmentId,
+              appointmentType: x.appointmentType,
+            }),
+          ),
+        ),
+      );
 
     logger.info(`commands ${JSON.stringify(_commands)}`);
     for (let c of _commands) {
@@ -33,10 +45,10 @@ module.exports = function(rsRepository,
     }
 
     ctx.status = 200;
-    ctx.body = {commandsProcessed: _commands};
+    ctx.body = { commandsProcessed: _commands };
   };
 
   return {
-    appointmentStatusUpdate
+    appointmentStatusUpdate,
   };
 };
