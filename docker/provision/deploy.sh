8d92baacab2aa158c7d492c90e3bb688e37356ac
Raif Harik <reharik@gmail.com>
2019-03-06 12:54:47 -0600

    updating the deploy scripts with proper ip

--- docker/provision/deploy.sh
+++ docker/provision/deploy.sh
@@ -7,7 +7,7 @@ echo "--------------------------------------"
 echo "Pushing compose file to server"
 echo "--------------------------------------"
 
-scp -r ./deploy/. ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com:~/
+scp -r ./deploy/. ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com:~/
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x docker-compose.yml
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x deploy_containers.sh
 
