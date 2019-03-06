13582c2f95e05ba502b962ebb4fa577a586d8b7f
Raif Harik <reharik@gmail.com>
2019-03-06 13:30:00 -0600

    forgot to change all the ips

--- docker/provision/deploy.sh
+++ docker/provision/deploy.sh
@@ -14,13 +14,13 @@ scp -o "StrictHostKeyChecking=no" -r ./deploy/. ubuntu@ec2-18-220-170-213.us-eas
 echo "--------------------------------------"
 echo "chmod on docker-compose"
 echo "--------------------------------------"
-ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x docker-compose.yml
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com chmod a+x docker-compose.yml
 echo "--------------------------------------"
 echo "chmod on deploy_container"
 echo "--------------------------------------"
-ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x deploy_containers.sh
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com chmod a+x deploy_containers.sh
 
 echo "--------------------------------------"
 echo "Running the compose file on server"
 echo "--------------------------------------"
-ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com ./deploy_containers.sh
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com ./deploy_containers.sh
