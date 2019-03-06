3bab7eeb25174065466c3778d965e24bf454fdcd
Raif Harik <reharik@gmail.com>
2019-03-06 13:11:53 -0600

    messing with ssh and scp now

--- docker/provision/deploy.sh
+++ docker/provision/deploy.sh
@@ -7,8 +7,17 @@ echo "--------------------------------------"
 echo "Pushing compose file to server"
 echo "--------------------------------------"
 
-scp -r ./deploy/. ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com:~/
+echo "--------------------------------------"
+echo "scp"
+echo "--------------------------------------"
+scp -o "StrictHostKeyChecking=no" -r ./deploy/. ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com:~/
+echo "--------------------------------------"
+echo "chmod on docker-compose"
+echo "--------------------------------------"
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x docker-compose.yml
+echo "--------------------------------------"
+echo "chmod on deploy_container"
+echo "--------------------------------------"
 ssh ubuntu@ec2-18-220-36-147.us-east-2.compute.amazonaws.com chmod a+x deploy_containers.sh
 
 echo "--------------------------------------"
