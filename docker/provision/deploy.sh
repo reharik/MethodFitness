24a6ce08ea60ed598e2e1eb591e3a174e41cf3ae
Raif Harik <reharik@gmail.com>
2019-03-07 10:08:31 -0600

    still didn't work trying to manually kill file before writing to it.

--- docker/provision/deploy.sh
+++ docker/provision/deploy.sh
@@ -12,9 +12,10 @@ echo "scp"
 ls -la $(pwd)/deploy/
 cat $(pwd)/deploy/.env
 echo "--------------------------------------"
-ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'cat > .env' < ./deploy/.env
-ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'cat > docker-compose.yml' < ./deploy/docker-compose.yml
-ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'cat > deploy_containers.sh' < ./deploy/deploy_containers.sh
+# I may need  -o "StrictHostKeyChecking=no"  here or some equivilant. 
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'rm .env; cat > .env' < ./deploy/.env
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'rm docker-compose.yml; cat > docker-compose.yml' < ./deploy/docker-compose.yml
+ssh ubuntu@ec2-18-220-170-213.us-east-2.compute.amazonaws.com 'rm deploy_containers.sh; cat > deploy_containers.sh' < ./deploy/deploy_containers.sh
 echo "--------------------------------------"
 echo "chmod on docker-compose"
 echo "--------------------------------------"
